fa6b0d77a6f5feb645b62670b5d0ee48f7712c5b
koshyj8 <koshyj88@gmail.com>
2023-07-25 21:27:49 +0400

    custom help command

--- DASABot/mainBot.py
+++ DASABot/mainBot.py
@@ -7,8 +7,6 @@ from discord.ext import commands
 from discord import app_commands
 import asyncio
 
-from pretty_help import PrettyHelp
-
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -16,8 +14,51 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 intents = discord.Intents.all()
 intents.message_content = True
 bot = commands.Bot(command_prefix="/",
-intents=intents,help_command=PrettyHelp(color = discord.Color.random(),
-                                        no_category = "Developer commands."))
+                    intents=intents)
+bot.remove_command('help')
+
+@bot.group(invoke_without_command = True)
+async def help(ctx):
+    em = discord.Embed(title = "DASA Bot Commands", description = "use /help <command> for information about commands", color = discord.Color.random())
+    em.add_field(name='</cutoff:1131246029531004968>',
+                    value = 'college, year, ciwg, round, branch(optional)',
+                    inline=False)
+    em.add_field(name='</analyse:1131969029968502918>',
+                    value='rank, ciwg, branch(optional)',
+                    inline=False)
+    em.add_field(name='</airport:1133054254203011082>',
+                    value='college',
+                    inline=False)
+    em.set_footer(text = "This message will be deleted after 1 minute.")
+    await ctx.send(embed = em, delete_after = 60)
+
+@help.command()
+async def cutoff(ctx):
+    em = discord.Embed(title='</cutoff:1131246029531004968>',
+                       description='Retrieve cutoffs for various colleges and/or branches', color=discord.Color.random())
+    em.add_field(name = "Syntax: ", value = "</cutoff:1131246029531004968> `<college> <year> <ciwg> <round> [branch]`", inline = True)
+    em.set_footer(text="This message will be deleted after 1 minute.")
+    await ctx.send(embed = em, delete_after = 60)
+
+
+@help.command()
+async def airport(ctx):
+    em = discord.Embed(
+        title='</airport:1133054254203011082>', description='Retrieves closest airport to the given college', color=discord.Color.random())
+    em.add_field(name="Syntax: ",
+                 value="</airport:1133054254203011082> `<college>`", inline=True)
+    em.set_footer(text="This message will be deleted after 1 minute.")
+    await ctx.send(embed=em, delete_after = 60)
+
+
+@help.command()
+async def analyse(ctx):
+    em = discord.Embed(
+        title='</analyse:1131969029968502918>', description='Retrieve cutoffs closes to your rank', color = discord.Color.random())
+    em.add_field(name="Syntax: ",
+                 value="</analyse:1131969029968502918> `<rank ><ciwg> [branch]`", inline=True)
+    em.set_footer(text="This message will be deleted after 1 minute.")
+    await ctx.send(embed=em, delete_after=60)
 
 @bot.event
 async def on_ready():
